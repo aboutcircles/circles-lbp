1364bafc687f6cd892ec7b880046dbb1dabf898a
roleengineer <roleengineer@gmail.com>
2025-01-12 04:29:15 +0100

    deployed and refactored api call

--- script/DeployFactory.s.sol
+++ script/DeployFactory.s.sol
@@ -6,7 +6,7 @@ import {CirclesBackingFactory} from "src/factory/CirclesBackingFactory.sol";
 
 contract DeployFactory is Script {
     address deployer = address(0x6BF173798733623cc6c221eD52c010472247d861);
-    CirclesBackingFactory public circlesBackingFactory;
+    CirclesBackingFactory public circlesBackingFactory; // 0xD608978aD1e1473fa98BaD368e767C5b11e3b3cE
 
     function setUp() public {}
 
@@ -19,3 +19,27 @@ contract DeployFactory is Script {
         console.log(address(circlesBackingFactory), "CirclesBackingFactory");
     }
 }
+
+/*
+curl -X 'POST' \
+  'https://api.cow.fi/xdai/api/v1/orders' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "sellToken": "0x2a22f9c3b484c3629090FeED35F17Ff8F88f76F0",
+  "buyToken": "0x6A023CCd1ff6F2045C3309768eAd9E68F978f6e1",
+  "receiver": "0xe75F06c807038D7D38e4f9716FF953eA1dA39157",
+  "sellAmount": "1000000",
+  "buyAmount": "1",
+  "validTo": 1894324190,
+  "feeAmount": "0",
+  "kind": "sell",
+  "partiallyFillable": false,
+  "sellTokenBalance": "erc20",
+  "buyTokenBalance": "erc20",
+  "signingScheme": "presign",
+  "signature": "0x",
+  "from": "0xe75F06c807038D7D38e4f9716FF953eA1dA39157",
+  "appData": "{\"version\":\"1.1.0\",\"appCode\":\"Circles backing powered by AboutCircles\",\"metadata\":{\"hooks\":{\"version\":\"0.1.0\",\"post\":[{\"target\":\"0xe75f06c807038d7d38e4f9716ff953ea1da39157\",\"callData\":\"0x13e8f89f\",\"gasLimit\":\"6000000\"}]}}}"
+}'
+*/
--- script/DeployOrderCreator.s.sol
+++ script/DeployOrderCreator.s.sol
@@ -20,27 +20,3 @@ contract DeployPrototype is Script {
         console.log(address(orderCreator), "orderCreator");
     }
 }
-
-/*
-curl -X 'POST' \
-  'https://api.cow.fi/xdai/api/v1/orders' \
-  -H 'accept: application/json' \
-  -H 'Content-Type: application/json' \
-  -d '{
-  "sellToken": "0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d",
-  "buyToken": "0x9C58BAcC331c9aa871AFD802DB6379a98e80CEdb",
-  "receiver": "0xEA39b6F8F98f91ECCe24A5601FD02DF850d3eC3E",
-  "sellAmount": "100000000000000000",
-  "buyAmount": "1",
-  "validTo": 1894006860,
-  "feeAmount": "0",
-  "kind": "sell",
-  "partiallyFillable": false,
-  "sellTokenBalance": "erc20",
-  "buyTokenBalance": "erc20",
-  "signingScheme": "presign",
-  "signature": "0x",
-  "from": "0xEA39b6F8F98f91ECCe24A5601FD02DF850d3eC3E",
-  "appData": "{\"version\":\"1.1.0\",\"appCode\":\"Zeal powered by Qantura\",\"metadata\":{\"hooks\":{\"version\":\"0.1.0\",\"post\":[{\"target\":\"0xea39b6f8f98f91ecce24a5601fd02df850d3ec3e\",\"callData\":\"0xbb5ae136\",\"gasLimit\":\"200000\"}]}}}"
-}'
-*/
