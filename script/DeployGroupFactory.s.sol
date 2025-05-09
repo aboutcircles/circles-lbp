d5a52e1681d01770afa09ae19f8cfa1ecce009f4
roleengineer <roleengineer@gmail.com>
2025-05-09 13:21:16 +0200

    (deployment): deployed group lbp factory on Gnosis chain

--- script/DeployGroupFactory.s.sol
+++ script/DeployGroupFactory.s.sol
@@ -8,8 +8,8 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IERC1155} from "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
 contract DeployFactory is Script {
-    address deployer = address(0x915aec9009a847a8EB1f65bA87dC02742E37B9D1);
-    GroupLBPFactory public groupLBPFactory; // 0xcA5dBf14434bE4F80b83A06AFCA402458B822abC
+    address deployer = address(0xaAb15A045e74c6539B696B115e763A22BE5C9594);
+    GroupLBPFactory public groupLBPFactory; // 0x3B36d73506C3e75FcaCB27340faA38ade1CBaF0a
 
     function setUp() public {}
 
